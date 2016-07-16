d2257acfddf2d0e7b364342977a0ab3186877c1f
dm4 <sunrisedm4@gmail.com>
2016-07-16 17:02:51 +0800

    Get username & pasword from environment variables.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 import check_mail
 
-USERNAME = 'user@example.com'
-PASSWORD = 'yourpassword'
+USERNAME = os.environ['USERNAME']
+PASSWORD = os.environ['PASSWORD']
 
 def dosomething(agent, portals):
     print(agent)
