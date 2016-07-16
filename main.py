a91c38b21c527c5e94876a75249b62d0ebf4816b
dm4 <sunrisedm4@gmail.com>
2016-07-16 22:08:38 +0800

    Add telepot for telegram bot.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import check_mail
+import telepot
 
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
@@ -10,5 +11,18 @@ def dosomething(agent, portals):
     print(agent)
     print(portals)
 
+def handle_telegram_message(message):
+    content_type, chat_type, chat_id = telepot.glance(message)
+    if content_type == 'text':
+        text = message['text']
+        print(text)
+
+# Run telepot
+if 'TELEGRAM_TOKEN' in os.environ:
+    bot = telepot.Bot(os.environ['TELEGRAM_TOKEN'])
+    bot.message_loop(handle_telegram_message)
+
+# Run mail checker
 check_mail.run(USERNAME, PASSWORD, callback=dosomething)
+
 input()
